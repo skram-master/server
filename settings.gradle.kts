388a06f5ef1a0e00474671c483ff364034a10c65
Rokichi <rokichi0906@gmail.com>
2024-11-01 01:25:58 +0900

    Add: catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,27 @@
-rootProject.name = "com.skram-master"
+rootProject.name = "com.skramMaster"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    versionCatalogs {
+        create("kotlinLibs") {
+            from(files("gradle/kotlin.versions.toml"))
+        }
+        create("ktorLibs") {
+            from(files("gradle/ktor.versions.toml"))
+        }
+        create("thirdPartyLibs") {
+            from(files("gradle/third-party.versions.toml"))
+        }
+    }
+}
