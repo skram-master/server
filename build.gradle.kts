2aa614f1e7904f8fb9d3cb0800fb74f940dcbb6b
Rokichi <rokichi0906@gmail.com>
2024-11-01 01:29:52 +0900

    Add: use catalog in plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,14 @@
 val appVersion: String by project
 
-val kotlin_version: String by project
-val logback_version: String by project
-val postgres_version: String by project
-val h2_version: String by project
-
 plugins {
-    kotlin("jvm") version "2.0.21"
-    id("io.ktor.plugin") version "3.0.0"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
+    application
+    alias(kotlinLibs.plugins.kotlin.jvm) apply (false)
+    alias(ktorLibs.plugins.io.ktor) apply (false)
+    alias(thirdPartyLibs.plugins.jooq.codegen) apply (false)
 }
 
 group = "com.skramMaster"
-version = "0.0.1"
+version = appVersion
 
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
@@ -26,24 +22,5 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-content-negotiation-jvm")
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-    implementation("io.ktor:ktor-server-auth-jvm")
-    implementation("io.ktor:ktor-server-http-redirect-jvm")
-    implementation("org.postgresql:postgresql:$postgres_version")
-    implementation("com.h2database:h2:$h2_version")
-    implementation("io.ktor:ktor-server-websockets-jvm")
-    implementation("io.ktor:ktor-server-cors-jvm")
-    implementation("io.ktor:ktor-server-csrf-jvm")
-    implementation("io.ktor:ktor-server-call-logging-jvm")
-    implementation("io.ktor:ktor-server-swagger-jvm")
-    implementation("io.ktor:ktor-server-default-headers-jvm")
-    implementation("io.ktor:ktor-server-resources-jvm")
-    implementation("io.ktor:ktor-server-auth-jwt-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-config-yaml")
-    testImplementation("io.ktor:ktor-server-test-host-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
 }
