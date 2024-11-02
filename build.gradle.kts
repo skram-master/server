3f4f102b3496b26f799873d83e83e0ee9f149469
Rokichi <rokichi0906@gmail.com>
2024-11-02 23:46:41 +0900

    Add: update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,22 +27,22 @@ repositories {
 
 dependencies {
     implementation(ktorLibs.ktor.server.core.jvm)
-    implementation(ktorLibs.ktor.server.content.negotiation.jvm)
-    implementation(ktorLibs.ktor.serialization.kotlinx.json.jvm)
+    implementation(ktorLibs.ktor.server.netty.jvm)
+    implementation(ktorLibs.ktor.server.config.yaml)
+
     implementation(ktorLibs.ktor.server.auth.jvm)
+    implementation(ktorLibs.ktor.server.default.headers.jvm)
+    implementation(ktorLibs.ktor.server.content.negotiation.jvm)
     implementation(ktorLibs.ktor.server.http.redirect.jvm)
     implementation(ktorLibs.ktor.server.websockets.jvm)
     implementation(ktorLibs.ktor.server.cors.jvm)
     implementation(ktorLibs.ktor.server.csrf.jvm)
-    implementation(ktorLibs.ktor.server.call.logging.jvm)
-    implementation(ktorLibs.ktor.server.default.headers.jvm)
-    implementation(ktorLibs.ktor.server.resources.jvm)
     implementation(ktorLibs.ktor.server.auth.jwt.jvm)
-    implementation(ktorLibs.ktor.server.netty.jvm)
-    implementation(ktorLibs.ktor.server.config.yaml)
-
-    implementation(thirdPartyLibs.ktor.swagger.ui)
+    implementation(ktorLibs.ktor.server.call.logging.jvm)
     implementation(thirdPartyLibs.logback.classic)
+    implementation(thirdPartyLibs.ktor.swagger.ui)
+    implementation(ktorLibs.ktor.server.resources.jvm)
+    implementation(ktorLibs.ktor.serialization.kotlinx.json.jvm)
 }
 
 ktor {
