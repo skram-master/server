5c8f7711d11ad4c5b245bc8eefc492a05710e145
Rokichi <rokichi0906@gmail.com>
2024-11-05 01:28:06 +0900

    Update: Application module

--- infrastructure/ktorapp/src/main/kotlin/com/skramMaster/infrastructure/ktorapp/Application.kt
+++ infrastructure/ktorapp/src/main/kotlin/com/skramMaster/infrastructure/ktorapp/Application.kt
@@ -1,23 +1,19 @@
 package com.skramMaster.infrastructure.ktorapp
 
-import com.skramMaster.infrastructure.ktorapp.plugin.configureKoin
-import com.skramMaster.infrastructure.ktorapp.plugin.configureMonitoring
-import com.skramMaster.infrastructure.ktorapp.plugin.configureSecurity
-import com.skramMaster.infrastructure.ktorapp.plugin.configureSerialization
-import com.skramMaster.infrastructure.ktorapp.plugin.configureSockets
-import com.skramMaster.infrastructure.ktorapp.plugin.configureSwaggerUI
+import com.skramMaster.infrastructure.ktorapp.plugin.*
 import io.ktor.server.application.Application
 
 fun main(args: Array<String>) = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused")
 fun Application.module() {
+    configureKoin()
     configureSecurity()
 //    configureHTTP()
     configureSockets()
     configureSerialization()
     configureMonitoring()
-    configureKoin()
+    configureDatabase()
     configureSwaggerUI()
     configureRouting()
 }
--- infrastructure/ktorapp/src/main/kotlin/com/skramMaster/infrastructure/ktorapp/plugin/Koin.kt
+++ infrastructure/ktorapp/src/main/kotlin/com/skramMaster/infrastructure/ktorapp/plugin/Koin.kt
@@ -1,8 +1,7 @@
 package com.skramMaster.infrastructure.ktorapp.plugin
 
 import com.skramMaster.infrastructure.ktorapp.di.database.DatabaseModuleBuilder
-import io.ktor.server.application.Application
-import io.ktor.server.application.install
+import io.ktor.server.application.*
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
