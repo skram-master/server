5e94d498141893b80881e6c3ac5e377476440380
Rokichi <rokichi0906@gmail.com>
2024-11-10 01:23:23 +0900

    Add: TransactionProvider

--- domain/src/main/kotlin/com/skramMaster/domain/transaction/TransactionProvider.kt
+++ domain/src/main/kotlin/com/skramMaster/domain/transaction/TransactionProvider.kt
@@ -1,7 +1,5 @@
 package com.skramMaster.domain.transaction
 
-import kotlinx.coroutines.Deferred
-
 interface TransactionProvider {
-    suspend fun <T> transaction(block: suspend () -> T): Deferred<T>
+    suspend fun <T> transaction(block: suspend () -> T): T
 }
