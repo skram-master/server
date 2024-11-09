fb8642670436655567345310e272dd54d8ce986b
Rokichi <rokichi0906@gmail.com>
2024-11-10 01:24:46 +0900

    Fix: ArticlesV1

--- infrastructure/ktorapp/src/main/kotlin/com/skramMaster/infrastructure/ktorapp/route/v1/ArticlesV1.kt
+++ infrastructure/ktorapp/src/main/kotlin/com/skramMaster/infrastructure/ktorapp/route/v1/ArticlesV1.kt
@@ -1,59 +1,48 @@
 package com.skramMaster.infrastructure.ktorapp.route.v1
 
+import com.skraMaster.controller.article.ArticleController
+import com.skraMaster.dto.article.ArticleGetV1Request
+import com.skraMaster.dto.article.ArticleV1Response
 import com.skramMaster.infrastructure.ktorapp.extension.get
-import io.ktor.http.HttpStatusCode
-import io.ktor.resources.Resource
-import io.ktor.server.response.respond
-import io.ktor.server.routing.Route
+import com.skramMaster.infrastructure.ktorapp.extension.post
+import io.ktor.http.*
+import io.ktor.resources.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import kotlinx.serialization.Serializable
 
 @Serializable
 @Resource("/articles")
-private data class ArticlesV1(val sort: SortOrder? = SortOrder.default) {
-    @Suppress("EnumEntryName")
-    enum class SortOrder {
-        new,
-        old,
-        default,
-    }
+class ArticlesV1 {
 
     @Serializable
-    @Resource("{id}")
-    data class Id(val parent: ArticlesV1 = ArticlesV1(), val id: Int)
+    @Resource("/new")
+    class New(val parent: ArticlesV1 = ArticlesV1())
 
     companion object {
-        val tags = listOf("api/v1/articles")
+        val tags = listOf("Articles")
     }
 }
 
-@Serializable
-private data class ArticlesResponse(val id: Int, val title: String, val content: String)
-
-fun Route.resourceRouteV1() {
-    getArticles()
-    getArticleById()
+fun Route.resourceRouteV1(articleController: ArticleController) {
+    getArticles(articleController = articleController)
+    createArticles(articleController = articleController)
 }
 
-private fun Route.getArticles() {
+private fun Route.getArticles(articleController: ArticleController) {
+
     get<ArticlesV1>(
         {
             tags = ArticlesV1.tags
-            request {
-                queryParameter<ArticlesV1.SortOrder>(ArticlesV1::sort.name) {
-                    required = false
-                    example("default") {
-                        value = ArticlesV1.SortOrder.default
-                    }
-                }
-            }
             response {
                 HttpStatusCode.OK to {
                     description = "Successful Request"
-                    body<List<ArticlesResponse>> {
+                    body<List<ArticleV1Response>> {
                         example("default") {
                             value = listOf(
-                                ArticlesResponse(1, "Article 1", "Content 1"),
-                                ArticlesResponse(2, "Article 2", "Content 2"),
+                                ArticleV1Response(1, "Article 1", "Content 1"),
+                                ArticleV1Response(2, "Article 2", "Content 2"),
                             )
                         }
                     }
@@ -63,48 +52,34 @@ private fun Route.getArticles() {
                 }
             }
         }
-    ) { articles ->
-        val articlesResponse = listOf(
-            ArticlesResponse(1, "Article 1", "Content 1"),
-            ArticlesResponse(2, "Article 2", "Content 2"),
-            ArticlesResponse(3, "Article 3", "Content 3")
-        ).let {
-            when (articles.sort) {
-                ArticlesV1.SortOrder.new -> it.sortedBy { it.id }
-                ArticlesV1.SortOrder.old -> it.sortedByDescending { it.id }
-                ArticlesV1.SortOrder.default -> it
-                else -> it
-            }
-        }
-        call.respond(articlesResponse)
+    ) {
+        call.respond(articleController.getArticles())
     }
 }
 
-fun Route.getArticleById() {
-    get<ArticlesV1.Id>(
+fun Route.createArticles(articleController: ArticleController) {
+    post<ArticlesV1.New>(
         {
             tags = ArticlesV1.tags
             request {
-                queryParameter<String>(ArticlesV1.Id::id.name) {
-                    description = "Get article by id"
-                    required = true
-                    example("default") {
-                        value = 1
-                    }
-                }
+                body<ArticleGetV1Request>()
             }
             response {
                 HttpStatusCode.OK to {
                     description = "Successful Request"
-                    body<ArticlesResponse> { }
+                    body<ArticleV1Response> {
+                        example("default") {
+                            value = ArticleV1Response(1, "Created Article 1", "Content 1")
+                        }
+                    }
                 }
                 HttpStatusCode.InternalServerError to {
                     description = "Something unexpected happened"
                 }
             }
         }
-    ) { articleId ->
-        val article = ArticlesResponse(articleId.id, "Article ${articleId.id}", "Content ${articleId.id}")
-        call.respond(article)
+    ) {
+        val request = call.receive<ArticleGetV1Request>()
+        call.respond(articleController.createArticle(request))
     }
 }
