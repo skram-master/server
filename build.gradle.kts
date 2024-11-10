3083be292948d124fb3b4e24b1f0dbd17feb3a2e
Rokichi <rokichi0906@gmail.com>
2024-11-11 01:42:09 +0900

    Add: detekt rule

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,18 @@
 plugins {
-    alias(kotlinLibs.plugins.kotlin.jvm) apply (false)
-    alias(thirdPartyLibs.plugins.detekt) apply (false)
+    alias(kotlinLibs.plugins.kotlin.jvm)
+    alias(thirdPartyLibs.plugins.detekt)
 }
 
 val appVersion: String by project
+
+detekt {
+    parallel = true
+
+    config.from(files("detekt/detekt.yml"))
+    buildUponDefaultConfig = true
+
+    basePath = rootDir.absolutePath
+}
 
 allprojects {
     version = appVersion
@@ -15,5 +24,19 @@ allprojects {
 
     tasks.withType<Test>().configureEach {
         useJUnitPlatform()
+    }
+}
+
+val reportMerge by tasks.registering(io.gitlab.arturbosch.detekt.report.ReportMergeTask::class) {
+    output.set(rootProject.layout.buildDirectory.file("reports/detekt/merge.html reports/detekt/merge.html"))
+}
+
+subprojects {
+    tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+        finalizedBy(reportMerge)
+    }
+
+    reportMerge {
+        input.from(tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().map { it.htmlReportFile })
     }
 }
