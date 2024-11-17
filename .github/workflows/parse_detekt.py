3de49976f45158da559a0888a4bc7be7dbb717f5
Rokichi <rokichi0906@gmail.com>
2024-11-18 00:56:06 +0900

    Update: python parser

--- .github/workflows/parse_detekt.py
+++ .github/workflows/parse_detekt.py
@@ -1,7 +1,35 @@
 import os
+import subprocess
+import xml.etree.ElementTree as ET
 
-report_path = os.environ.get("DETEKT_REPORT_PATH")
+report_path = os.environ.get(
+    "DETEKT_REPORT_PATH", "build/reports/detekt/merge.xml"
+)
 
 
 def main():
-    print(f"Detekt report path: {report_path}")
+    errors: list[str] = []
+    root = ET.parse(report_path).getroot()
+    for file in root.findall("file"):
+        file_name = file.get('name')
+        file_errors = []
+        for error in file.findall('error'):
+            column = error.get('column')
+            line = error.get('line')
+            message = error.get('message')
+            source = error.get('source')
+            each_result = f"### {source}: {message}</br>\tcolumn: {column} line: {line}"
+            file_errors.append(each_result)
+        if file_errors:
+            result = f"## {file_name}</br>"
+            result += "</br>".join(file_errors)
+            errors.append(result)
+    if errors:
+        result_str = "</br></br>".join(errors)
+        subprocess.run(
+            [f"echo '{result_str}' >> $GITHUB_STEP_SUMMARY"], shell=True
+        )
+
+
+if __name__ == "__main__":
+    main()
