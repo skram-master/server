a1e898f1928daea145fdc82e5aae927a495405be
Rokichi <rokichi0906@gmail.com>
2025-02-09 20:03:36 +0900

    Update: ktor and exposed version and add flyway

--- infrastructure/datasource/build.gradle.kts
+++ infrastructure/datasource/build.gradle.kts
@@ -4,12 +4,18 @@ plugins {
 
 dependencies {
     implementation(kotlinLibs.kotlin.coroutine)
+
     implementation(thirdPartyLibs.postgres)
+
     implementation(thirdPartyLibs.exposed.core)
     implementation(thirdPartyLibs.exposed.crypt)
     implementation(thirdPartyLibs.exposed.dao)
     implementation(thirdPartyLibs.exposed.jdbc)
     implementation(thirdPartyLibs.exposed.kotlin.datetime)
+    implementation(thirdPartyLibs.exposed.json)
+    implementation(thirdPartyLibs.exposed.migration)
+
+    implementation(thirdPartyLibs.flyway.core)
 
     implementation(project(":domain"))
 }
