1f39ac9cd0267586c36834fd9559ed1b00e81dc6
Rokichi <rokichi0906@gmail.com>
2025-02-10 04:02:31 +0900

    Add: RoomSettingsController

--- infrastructure/ktorapp/src/main/kotlin/com/skram_master/infrastructure/ktorapp/di/controller/ControllerDIModuleBuilder.kt
+++ infrastructure/ktorapp/src/main/kotlin/com/skram_master/infrastructure/ktorapp/di/controller/ControllerDIModuleBuilder.kt
@@ -1,10 +1,12 @@
 package com.skram_master.infrastructure.ktorapp.di.controller
 
 import com.skram_master.controller.article.ArticleController
+import com.skram_master.controller.room_settings.RoomSettingsController
 import org.koin.dsl.module
 
 object ControllerDIModuleBuilder {
     fun build() = module {
         factory { ArticleController(get()) }
+        factory { RoomSettingsController(get()) }
     }
 }
