3ec221938df89868f7957ef1ff2a1ea0b30a1351
Rokichi <rokichi0906@gmail.com>
2025-02-13 01:27:45 +0900

    Fix: rename convertToSnakeCase

--- infrastructure/datasource/src/main/kotlin/com/skram_master/infrastructure/datasource/entity/TimestampTables.kt
+++ infrastructure/datasource/src/main/kotlin/com/skram_master/infrastructure/datasource/entity/TimestampTables.kt
@@ -9,8 +9,7 @@ import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.kotlin.datetime.datetime
 
 private interface ConvertToSnakeCase {
-    // TODO: rename
-    fun getTableName(tableName: String): String {
+    fun convertToSnakeCase(tableName: String): String {
         return tableName.replace(Regex("([a-z])([A-Z])")) {
             "${it.groupValues[1]}_${it.groupValues[2]}"
         }.lowercase()
@@ -19,7 +18,7 @@ private interface ConvertToSnakeCase {
 
 // TODO: implement auto update
 internal abstract class TimestampIntIdTable(tableName: String) :
-    IntIdTable(getTableName(tableName)) {
+    IntIdTable(convertToSnakeCase(tableName)) {
 
     val createdAt =
         datetime("created_at").clientDefault { Clock.System.now().toLocalDateTime(TimeZone.UTC) }
@@ -30,7 +29,7 @@ internal abstract class TimestampIntIdTable(tableName: String) :
 }
 
 internal abstract class TimestampLongIdTable(tableName: String) :
-    LongIdTable(getTableName(tableName)) {
+    LongIdTable(convertToSnakeCase(tableName)) {
 
     val createdAt =
         datetime("created_at").clientDefault { Clock.System.now().toLocalDateTime(TimeZone.UTC) }
