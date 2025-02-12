3bdeb97c8665794fd18a22ddd38f0f8a405e3468
Rokichi <rokichi0906@gmail.com>
2025-02-13 08:06:38 +0900

    Add: Extend update and ExtendUpdateTest

--- infrastructure/datasource/src/main/kotlin/com/skram_master/infrastructure/datasource/entity/utils/ExtendUpdate.kt
+++ infrastructure/datasource/src/main/kotlin/com/skram_master/infrastructure/datasource/entity/utils/ExtendUpdate.kt
@@ -1,6 +1,5 @@
 package com.skram_master.infrastructure.datasource.entity.utils
 
-import com.skram_master.infrastructure.datasource.entity.TimestampTable
 import kotlinx.datetime.Clock
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
@@ -12,20 +11,20 @@ import org.jetbrains.exposed.sql.statements.UpdateStatement
 import org.jetbrains.exposed.sql.update
 import org.jetbrains.exposed.sql.updateReturning
 
-private inline fun <T : TimestampTable<*>> T.bodyWithUpdateAt(
-    crossinline body: T.(UpdateStatement) -> Unit,
+private fun <T : TimestampTable<*>> T.bodyWithUpdateAt(
+    body: T.(UpdateStatement) -> Unit,
 ): (T.(UpdateStatement) -> Unit) {
     return { statement ->
-        statement[this@bodyWithUpdateAt.updatedAt] =
+        statement[updatedAt] =
             Clock.System.now().toLocalDateTime(TimeZone.UTC)
         body(this@bodyWithUpdateAt, statement)
     }
 }
 
-internal inline fun <T : TimestampTable<*>> T.updateWithUpdatedAt(
+internal fun <T : TimestampTable<*>> T.updateWithUpdatedAt(
     where: SqlExpressionBuilder.() -> Op<Boolean>,
     limit: Int? = null,
-    crossinline body: T.(UpdateStatement) -> Unit,
+    body: T.(UpdateStatement) -> Unit,
 ): Int {
     return update(
         where = where,
@@ -34,9 +33,10 @@ internal inline fun <T : TimestampTable<*>> T.updateWithUpdatedAt(
     )
 }
 
-internal inline fun <T : TimestampTable<*>> T.updateWithUpdatedAt(
+@Deprecated("Postgres does not support limit in update statement")
+internal fun <T : TimestampTable<*>> T.updateWithUpdatedAt(
     limit: Int? = null,
-    crossinline body: T.(UpdateStatement) -> Unit,
+    body: T.(UpdateStatement) -> Unit,
 ): Int {
     return update(
         limit = limit,
@@ -44,10 +44,10 @@ internal inline fun <T : TimestampTable<*>> T.updateWithUpdatedAt(
     )
 }
 
-internal inline fun <T : TimestampTable<*>> T.updateReturningWithUpdatedAt(
+internal fun <T : TimestampTable<*>> T.updateReturningWithUpdatedAt(
     returning: List<Expression<*>> = columns,
     where: SqlExpressionBuilder.() -> Op<Boolean>,
-    crossinline body: T.(UpdateStatement) -> Unit,
+    body: T.(UpdateStatement) -> Unit,
 ): ReturningStatement {
     return updateReturning(
         returning = returning,
@@ -56,9 +56,9 @@ internal inline fun <T : TimestampTable<*>> T.updateReturningWithUpdatedAt(
     )
 }
 
-internal inline fun <T : TimestampTable<*>> T.updateReturningWithUpdatedAt(
+internal fun <T : TimestampTable<*>> T.updateReturningWithUpdatedAt(
     returning: List<Expression<*>> = columns,
-    crossinline body: T.(UpdateStatement) -> Unit,
+    body: T.(UpdateStatement) -> Unit,
 ): ReturningStatement {
     return updateReturning(
         returning = returning,
---
+++ infrastructure/datasource/src/test/kotlin/com/skram_master/infrastructure/datasource/entity/utils/ExtendUpdateTest.kt
@@ -0,0 +1,120 @@
+package com.skram_master.infrastructure.datasource.entity.utils
+
+import com.skram_master.core.coroutine.CoroutineProvider
+import com.skram_master.infrastructure.datasource.database.DefaultTransactionProvider
+import com.skram_master.infrastructure.datasource.database.TestDatabaseFactory
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.comparables.shouldBeGreaterThan
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.selectAll
+
+internal object TestIntTable : TimestampIntIdTable("TestIntTable") {
+    val name = varchar("name", 255)
+}
+
+internal object TestLongTable : TimestampLongIdTable("TestLongTable") {
+    val name = varchar("name", 255)
+}
+
+internal object TestUUIDTable : TimestampUUIDTable("TestUUIDTable") {
+    val name = varchar("name", 255)
+}
+
+@OptIn(ExperimentalCoroutinesApi::class)
+internal class ExtendUpdateTest : ShouldSpec({
+
+    val tables = arrayOf(TestIntTable, TestLongTable, TestUUIDTable)
+    val testDispatcher = StandardTestDispatcher()
+    val databaseFactory = TestDatabaseFactory(
+        initBlock = { SchemaUtils.create(*tables) },
+        resetBlock = { SchemaUtils.drop(*tables) },
+    )
+    val transactionProvider = DefaultTransactionProvider(databaseFactory)
+
+    beforeSpec { spec ->
+        CoroutineProvider.IO.dispatcherHandler = { testDispatcher }
+        spec.coroutineTestScope = true
+        databaseFactory.reset()
+        databaseFactory.init()
+    }
+
+    afterSpec { spec ->
+        CoroutineProvider.IO.reset()
+    }
+
+    context("custom update* function updates the updatedAt column when it is called") {
+        should("bodyWithUpdateAt should update the updatedAt column") {
+            // given
+            val testIntTable = transactionProvider.transaction {
+                TestIntTable.insert {
+                    it[name] = "test"
+                }
+            }.await()
+
+            // when
+            val updatedId = transactionProvider.transaction {
+                TestIntTable.updateWithUpdatedAt(
+                    where = { TestIntTable.id eq testIntTable[TestIntTable.id] },
+                    body = { statement ->
+                        statement[name] = "updated"
+                    },
+                )
+            }.await()
+
+            // then
+            val updatedRecord = transactionProvider.transaction {
+                TestIntTable.selectAll().where { TestIntTable.id eq updatedId }.single()
+            }.await()
+
+            updatedRecord[TestIntTable.updatedAt] shouldBeGreaterThan testIntTable[TestIntTable.updatedAt]
+        }
+
+        should("updateReturningWithUpdatedAt should update the updatedAt column") {
+            // given
+            val testLongTable = transactionProvider.transaction {
+                TestLongTable.insert {
+                    it[name] = "test"
+                }
+            }.await()
+
+            // when
+            val updatedAtOfUpdateRecord = transactionProvider.transaction {
+                TestLongTable.updateReturningWithUpdatedAt(
+                    returning = listOf(TestLongTable.updatedAt),
+                    where = { TestLongTable.id eq testLongTable[TestLongTable.id] },
+                    body = { statement ->
+                        statement[name] = "updated"
+                    },
+                ).single()
+            }.await()
+
+            // then
+            updatedAtOfUpdateRecord[TestLongTable.updatedAt] shouldBeGreaterThan testLongTable[TestLongTable.updatedAt]
+        }
+
+        should("updateReturningWithUpdatedAt excluded where should update the updatedAt column") {
+            // given
+            val testUUIDTable = transactionProvider.transaction {
+                TestUUIDTable.insert {
+                    it[name] = "test"
+                }
+            }.await()
+
+            // when
+            val updatedAtOfUpdateRecord = transactionProvider.transaction {
+                TestUUIDTable.updateReturningWithUpdatedAt(
+                    returning = listOf(TestUUIDTable.updatedAt),
+                    body = { statement ->
+                        statement[name] = "updated"
+                    },
+                ).single()
+            }.await()
+
+            // then
+            updatedAtOfUpdateRecord[TestUUIDTable.updatedAt] shouldBeGreaterThan testUUIDTable[TestUUIDTable.updatedAt]
+        }
+    }
+})
